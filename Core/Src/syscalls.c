bf09c46e2de0a582d89be3e8537baabbfa4b1fbc
samuel <scesario@ycp.edu>
2021-02-25 15:55:17 -0500

    Switch to uart4

--- Core/Src/syscalls.c
+++ Core/Src/syscalls.c
@@ -94,7 +94,7 @@ return len;
 #include  <sys/unistd.h> // STDOUT_FILENO, STDERR_FILENO
 #include <stm32g4xx_hal.h>
 
-extern UART_HandleTypeDef huart1;
+extern UART_HandleTypeDef huart4;
 int _write(int file, char *data, int len)
 {
     if ((file != STDOUT_FILENO) && (file != STDERR_FILENO))
@@ -105,7 +105,7 @@ int _write(int file, char *data, int len)
 
     // arbitrary timeout 1000
     HAL_StatusTypeDef status =
-            HAL_UART_Transmit(&huart1, (uint8_t*)data, len, 1000);
+            HAL_UART_Transmit(&huart4, (uint8_t*)data, len, 1000);
 
     // return # of bytes written - as best we can tell
     return (status == HAL_OK ? len : 0);
