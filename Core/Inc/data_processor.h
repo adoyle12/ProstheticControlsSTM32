cfcb728179e8da55e96c601afdd2472329824584
dmoore29 <dmoore29@ycp.edu>
2022-03-07 09:46:34 -0800

    Remove unused code

--- Core/Inc/data_processor.h
+++ Core/Inc/data_processor.h
@@ -10,7 +10,5 @@
 #define BASE_VELOCITY_DELAY 15
 
 int DataProcessor_CheckThreshold(uint16_t half_buffer[4096], int startIndex, int stopIndex);
-void delay(float number_of_seconds);
-
 
 #endif //PROSTHETICCONTROLSSTM32_DATA_PROCESSOR_H
--- Core/Src/data_processor.c
+++ Core/Src/data_processor.c
@@ -1,51 +1,34 @@
 #include "data_processor.h"
 #include <stm32g4xx_hal.h>
 #include <stdio.h>
-#include <time.h>
 #include "hand.h"
 
 int DataProcessor_CheckThreshold(uint16_t half_buffer[4096], int startIndex, int stopIndex){
     while (startIndex < stopIndex){ // Iterate through half of buffer
-        int stateChanged = 0;
+        int hasMoved = 0; // Used to delay only after a movement
         if(half_buffer[startIndex] >= UPPER_THRESHOLD){ // Clench
             for(int i = 0; i<5; i++){
                 if(FingerPositions[i] > CLENCHED_FINGER_POSITION){ // If this finger is not clenched, clench it
-                    stateChanged = 1;
+                    hasMoved = 1;
                     Hand_Move(FingerPositions[0] - 1, (int[1]) {i}, 1);
 //                    printf("%i passed upper threshold of %i. Clenching... \r\n", half_buffer[startIndex], UPPER_THRESHOLD);
                 }
             }
-            if(stateChanged == 1){
+            if(hasMoved == 1){ // Only delay if a finger has moved
                 HAL_Delay(BASE_VELOCITY_DELAY);
             }
         } else if(half_buffer[startIndex] < LOWER_THRESHOLD){ // Release
             for(int i = 0; i<5; i++){
                 if(FingerPositions[i] < RELEASED_FINGER_POSITION){ // If this finger is not released, release it
-                    stateChanged = 1;
+                    hasMoved = 1;
                     Hand_Move(FingerPositions[0] + 1, (int[1]) {i}, 1);
 //                    printf("%i passed lower threshold of %i. Releasing... \r\n", half_buffer[startIndex], LOWER_THRESHOLD);
                 }
             }
-            if(stateChanged == 1){
+            if(hasMoved == 1){ // Only delay if a finger has moved
                 HAL_Delay(BASE_VELOCITY_DELAY);
             }
         }
         startIndex++;
     }
-}
-
-void delay(float number_of_seconds)
-{
-    // Converting time into milli_seconds
-    int milli_seconds = 1000 * number_of_seconds;
-
-    // Storing start time
-    clock_t start_time = clock();
-
-    printf("Waiting for %f seconds \r\n", number_of_seconds);
-
-    // looping till required time is not achieved
-    while (clock() < start_time + milli_seconds)
-        ;
-    printf("Done waiting... \r\n");
 }
