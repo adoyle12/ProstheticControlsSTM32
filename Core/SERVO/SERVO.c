27b79f06126983392422fa8e15162cbf55f31406
Alex Doyle <adoyle4@ycp.edu>
2022-03-30 12:43:14 -0400

    - Added CCR min & max calculations - Added check to RawMove() to keep servo in bounds

--- Core/SERVO/SERVO.c
+++ Core/SERVO/SERVO.c
@@ -16,8 +16,8 @@
 
 typedef struct
 {
-	uint16_t  Period_Min;
-	uint16_t  Period_Max;
+	uint16_t  CCR_Min;
+	uint16_t  CCR_Max;
 }SERVO_info;
 
 static SERVO_info gs_SERVO_info[SERVO_NUM] = {0};
@@ -26,8 +26,8 @@ static SERVO_info gs_SERVO_info[SERVO_NUM] = {0};
 void SERVO_Init(uint16_t au16_SERVO_Instance)
 {
     uint32_t ARR_Value = 255;
-    gs_SERVO_info[au16_SERVO_Instance].Period_Min = (uint16_t) (ARR_Value * (SERVO_CfgParam[au16_SERVO_Instance].MinPulse/3.0));
-    gs_SERVO_info[au16_SERVO_Instance].Period_Max = (uint16_t) (ARR_Value * (SERVO_CfgParam[au16_SERVO_Instance].MaxPulse/3.0));
+    gs_SERVO_info[au16_SERVO_Instance].CCR_Min = (uint16_t) (ARR_Value * (SERVO_CfgParam[au16_SERVO_Instance].MinPulse/3.0));
+    gs_SERVO_info[au16_SERVO_Instance].CCR_Max = (uint16_t) (ARR_Value * (SERVO_CfgParam[au16_SERVO_Instance].MaxPulse / 3.0));
 	HAL_TIM_PWM_Start(SERVO_CfgParam[au16_SERVO_Instance].TIM_Instance, SERVO_CfgParam[au16_SERVO_Instance].PWM_TIM_CH);
     printf("PWM Started on Servo %d \r\n",au16_SERVO_Instance);
 }
@@ -36,8 +36,8 @@ void SERVO_Init(uint16_t au16_SERVO_Instance)
 void SERVO_MoveTo(uint16_t au16_SERVO_Instance, float af_Angle)
 {
 	uint16_t au16_Pulse = 0;
-	au16_Pulse = ((af_Angle*(gs_SERVO_info[au16_SERVO_Instance].Period_Max - gs_SERVO_info[au16_SERVO_Instance].Period_Min))/180.0)
-			+ gs_SERVO_info[au16_SERVO_Instance].Period_Min;
+	au16_Pulse = ((af_Angle*(gs_SERVO_info[au16_SERVO_Instance].CCR_Max - gs_SERVO_info[au16_SERVO_Instance].CCR_Min)) / 180.0)
+			+ gs_SERVO_info[au16_SERVO_Instance].CCR_Min;
 
 	*(SERVO_CfgParam[au16_SERVO_Instance].TIM_CCRx) = au16_Pulse;
 }
@@ -47,20 +47,23 @@ void SERVO_RawMove(uint16_t au16_SERVO_Instance, uint16_t au16_Pulse)
 {
 
 //    printf("Raw Move: Servo %d, Pulse: %d  \r\n",au16_SERVO_Instance,au16_Pulse);
-    *(SERVO_CfgParam[au16_SERVO_Instance].TIM_CCRx) = au16_Pulse;
+    if (au16_Pulse >= gs_SERVO_info[au16_SERVO_Instance].CCR_Min && au16_Pulse <= gs_SERVO_info[au16_SERVO_Instance].CCR_Max)
+    {
+        *(SERVO_CfgParam[au16_SERVO_Instance].TIM_CCRx) = au16_Pulse;
+    }
 }
 
 /* Gets The Maximum Pulse Width Value For A Specific Motor */
 uint16_t SERVO_Get_MaxPulse(uint16_t au16_SERVO_Instance)
 {
-	return (gs_SERVO_info[au16_SERVO_Instance].Period_Max);
+	return (gs_SERVO_info[au16_SERVO_Instance].CCR_Max);
 }
 
 
 /* Gets The Minimum Pulse Width Value For A Specific Motor */
 uint16_t SERVO_Get_MinPulse(uint16_t au16_SERVO_Instance)
 {
-	return (gs_SERVO_info[au16_SERVO_Instance].Period_Min);
+	return (gs_SERVO_info[au16_SERVO_Instance].CCR_Min);
 }
 
 
