d10e6f1d1f6ee332ba7f2bff425626d3ef8527a8
dmoore29 <dmoore29@ycp.edu>
2022-04-04 10:04:12 -0700

    Added thresholds to data processor

--- Core/Src/data_processor.c
+++ Core/Src/data_processor.c
@@ -7,19 +7,19 @@
 int DataProcessor_CheckThreshold(uint16_t* half_buffer, int startIndex, int stopIndex){
     while (startIndex < stopIndex){ // Iterate through half of buffer
         int hasMoved = 0; // Used to delay only after a movement
-        if(half_buffer[startIndex] >= MAX_THRESHOLD){ // Clench
+        if(half_buffer[startIndex] >= maxThreshold){ // Clench
             if(FingerPositions[2] < SERVO_Get_MaxPulse(2)){ // Finger that clenches the farthest
                 Hand_Move(FingerPositions[2] + 1, (int[5]) {0, 1, 2, 3, 4}, 5);
                 HAL_Delay(5);
 //                    printf("%i passed upper threshold of %i. Clenching... \r\n", half_buffer[startIndex], UPPER_THRESHOLD);
             }
-        } else if(half_buffer[startIndex] >= UPPER_THRESHOLD){ // Clench
+        } else if(half_buffer[startIndex] >= middleThreshold){ // Clench
             if(FingerPositions[2] < SERVO_Get_MaxPulse(2)){ // Finger that clenches the farthest
                 Hand_Move(FingerPositions[2] + 1, (int[5]) {0, 1, 2, 3, 4}, 5);
                 HAL_Delay(BASE_VELOCITY_DELAY);
 //                    printf("%i passed upper threshold of %i. Clenching... \r\n", half_buffer[startIndex], UPPER_THRESHOLD);
             }
-        } else if(half_buffer[startIndex] < LOWER_THRESHOLD){ // Release
+        } else if(half_buffer[startIndex] < minThreshold){ // Release
             if(FingerPositions[0] > SERVO_Get_MinPulse(2)){ // If this finger is not released, release it
                 hasMoved = 1;
                 Hand_Move(FingerPositions[0] - 1, (int[5]) {0, 1, 2, 3, 4}, 5);
